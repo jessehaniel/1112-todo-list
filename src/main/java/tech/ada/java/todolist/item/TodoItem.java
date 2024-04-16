ae4c319c5a010b5098a582e91222390e56e74028
Jessé Haniel <jesse.haniel@tjpb.jus.br>
2024-04-16 19:53:25 -0300

    Transformando em API REST

--- src/main/java/tech/ada/java/todolist/item/TodoItem.java
+++ src/main/java/tech/ada/java/todolist/item/TodoItem.java
@@ -2,7 +2,15 @@ package tech.ada.java.todolist.item;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public final class TodoItem {
 
     private String titulo;
@@ -11,62 +19,10 @@ public final class TodoItem {
     private final UUID uuid;
     private Boolean concluido;
 
-    public TodoItem(String titulo, String detalhes, LocalDateTime data, UUID uuid, Boolean concluido) {
-        this.titulo = titulo;
-        this.detalhes = detalhes;
-        this.data = data;
-        this.uuid = uuid;
-        this.concluido = concluido;
-    }
 
     public TodoItem(String titulo, String detalhes, LocalDateTime data) {
         this(titulo, detalhes, data, UUID.randomUUID(), false);
     }
 
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public Boolean getConcluido() {
-        return concluido;
-    }
-
-    public void setConcluido(Boolean concluido) {
-        this.concluido = concluido;
-    }
-
-    public LocalDateTime getData() {
-        return data;
-    }
-
-    public void setData(LocalDateTime data) {
-        this.data = data;
-    }
-
-    public String getDetalhes() {
-        return detalhes;
-    }
-
-    public void setDetalhes(String detalhes) {
-        this.detalhes = detalhes;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    @Override
-    public String toString() {
-        return "TodoItem[" +
-            "titulo=" + titulo + ", " +
-            "detalhes=" + detalhes + ", " +
-            "data=" + data + ", " +
-            "uuid=" + uuid + ", " +
-            "concluido=" + concluido + ']';
-    }
 
 }
--- src/main/java/tech/ada/java/todolist/item/TodoItemController.java
+++ src/main/java/tech/ada/java/todolist/item/TodoItemController.java
@@ -5,8 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import tech.ada.java.todolist.NaoEncontradoException;
 
+@RestController
+@RequestMapping("/todo-itens")
 public class TodoItemController {
 
     private final List<TodoItem> todoItemList = new ArrayList<>();
@@ -34,17 +48,20 @@ public class TodoItemController {
     //6. Marcar como concluído
     //7. Excluir
 
+    @GetMapping
     public List<TodoItem> listar() {
         return this.todoItemList;
     }
 
-    public List<TodoItem> consultarPorTitulo(String titulo) {
+    @GetMapping(value = "consultar", params = "titulo")
+    public List<TodoItem> consultarPorTitulo(@RequestParam String titulo) {
         return this.todoItemList.stream()
             .filter(it -> it.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
             .toList();
     }
 
-    public TodoItem buscarPorUuid(UUID uuid) {
+    @GetMapping("/{uuid}")
+    public TodoItem buscarPorUuid(@PathVariable UUID uuid) {
         return buscarPorUuidOptional(uuid)
             .orElseThrow(NaoEncontradoException::new);
     }
@@ -55,11 +72,15 @@ public class TodoItemController {
             .findFirst();
     }
 
-    public void adicionar(TodoItem item) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar(@RequestBody TodoItem item) {
         this.todoItemList.add(item);
     }
 
-    public void atualizar(UUID uuid, TodoItem itemAtualizado) {
+    @PutMapping("/{uuid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable UUID uuid, @RequestBody TodoItem itemAtualizado) {
         int index = this.buscarIndicePorUuid(uuid);
         if (index == -1) {
             throw new NaoEncontradoException();
@@ -76,12 +97,15 @@ public class TodoItemController {
         return -1;
     }
 
-    public void marcarComoConcluido(UUID uuid) {
+    @PatchMapping("/{uuid}/concluir")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void marcarComoConcluido(@PathVariable UUID uuid) {
         TodoItem item = buscarPorUuid(uuid);
         item.setConcluido(true);
     }
 
-    public void excluir(UUID uuid) {
+    @DeleteMapping("/{uuid}")
+    public void excluir(@PathVariable UUID uuid) {
         TodoItem item = buscarPorUuid(uuid);
         this.todoItemList.remove(item);
     }
