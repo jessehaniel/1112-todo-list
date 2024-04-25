9c560ab023386e33c1f442f593f567a1f128a136
Jessé Haniel <jesse.haniel@tjpb.jus.br>
2024-04-25 20:51:32 -0300

    Implementação da autenticação com JWT

---
+++ src/main/java/tech/ada/java/todolist/security/jwt/JwtAuthFilter.java
@@ -0,0 +1,56 @@
+package tech.ada.java.todolist.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String jwt = header.substring(7);
+        this.authenticateUserFromToken(request, jwt);
+        filterChain.doFilter(request, response);
+    }
+
+    private void authenticateUserFromToken(HttpServletRequest request, String jwt) {
+        final String username = this.jwtService.extractUsername(jwt);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if (this.jwtService.isTokenValid(jwt, userDetails)){
+                this.setAuthTokenOnSecurityContext(request, userDetails);
+            }
+        }
+    }
+
+    private void setAuthTokenOnSecurityContext(HttpServletRequest request, UserDetails userDetails) {
+        var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+
+}
---
+++ src/main/java/tech/ada/java/todolist/security/jwt/JwtService.java
@@ -0,0 +1,72 @@
+package tech.ada.java.todolist.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secret;
+
+    public String createToken(UserDetails userDetails) {
+        final var now = LocalDateTime.now();
+        return Jwts.builder()
+            .claims(new HashMap<>())
+            .subject(userDetails.getUsername())
+            .issuedAt(convertFromLocalDateTime(now))
+            .expiration(convertFromLocalDateTime(now.plusHours(1L)))
+            .signWith(this.getSignKey())
+            .compact();
+    }
+
+    private static Date convertFromLocalDateTime(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(Base64.getEncoder().encode(this.secret.getBytes()));
+    }
+
+    public String extractUsername(String token) {
+        return this.extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> resolver) {
+        final var claims = this.extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+            .verifyWith(this.getSignKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final var username = this.extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return this.extractExpiration(token).before(new Date(System.currentTimeMillis()));
+    }
+
+    private Date extractExpiration(String token) {
+        return this.extractClaims(token, Claims::getExpiration);
+    }
+
+}
